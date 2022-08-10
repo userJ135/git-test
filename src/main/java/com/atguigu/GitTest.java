6cf9ee78d84f263ef485078a31b72463441780c3
userJ <3096557473@qq.com>
2022-08-10 10:40:54 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello gitTwo!");
         System.out.println("hello gitThree!");
+        System.out.println("hello gitFour!");
     }
 }
