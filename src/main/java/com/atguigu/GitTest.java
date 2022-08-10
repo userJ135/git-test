26c1cf99937630d760a4b58b2b59cfe7a7c60b9e
userJ135 <104342608+userJ135@users.noreply.github.com>
2022-08-11 07:30:12 +0800

    pull commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello gitSix!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
